e76997dce1775a4ceb9f39fb18d3d090806422ff
GitMyDik <57413326+GitMyDik@users.noreply.github.com>
2024-12-05 13:15:44 +0200

    c

--- AA2024S/K5/Kodu5.java
+++ AA2024S/K5/Kodu5.java
@@ -1,114 +1,300 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/***********************************
+ * Algoritmid ja andmestruktuurid. LTAT.03.005
+ * 2024/2025 kevadsemester
+ *
+ * Kodutöö nr 5
+ * Teema: AVL-puud
+ *
+ * Autor: Johannes Tammerand
+ *
+ **********************************/
+
 public class Kodu5 {
 
+    /**
+     * Lisab kirje AVL-puusse
+     * @param juur AVL-puu juur
+     * @param väärtus Lisatav kirje
+     * @return Uus AVL-puu
+     */
     public static KOTipp lisaKirje(KOTipp juur, int väärtus) {
-        juur = lisaRek(juur, väärtus);
-
-        return juur;
-    }
-
-    public static KOTipp lisaRek(KOTipp juur, int väärtus){
         if (juur == null){
             KOTipp temp = new KOTipp(väärtus);
             temp.x = 1;
             return temp;
         }
 
+        int tasakaal;
         if (väärtus >= juur.väärtus){
-            juur.p = lisaRek(juur.p, väärtus);
+            juur.p = lisaKirje(juur.p, väärtus);
             if (juur.v != null){
-                juur.v.x = leiaTasakaal(juur.v);
-                juur.x = juur.v.x - juur.p.x;
+                juur.v.x = leiaKõrgus(juur.v);
+                juur.x = Math.max(juur.v.x, juur.p.x) + 1;
+                tasakaal = juur.v.x - juur.p.x;
             } else {
                 juur.x = juur.p.x + 1;
+                tasakaal = -juur.p.x;
             }
         } else {
-            juur.v = lisaRek(juur.v, väärtus);
+            juur.v = lisaKirje(juur.v, väärtus);
             if (juur.p != null){
-                juur.p.x = leiaTasakaal(juur.p);
-                juur.x = juur.v.x - juur.p.x;
+                juur.p.x = leiaKõrgus(juur.p);
+                juur.x = Math.max(juur.v.x, juur.p.x) + 1;
+                tasakaal = juur.v.x - juur.p.x;
             } else {
                 juur.x = juur.v.x + 1;
+                tasakaal = juur.v.x;
             }
         }
+
+        if (tasakaal == 2){
+            if (juur.v.p == null || (juur.v.v != null && leiaKõrgus(juur.v.v) - leiaKõrgus(juur.v.p) >= 0)){
+                juur = paremPööre(juur);
+            } else {
+                juur.v = vasakPööre(juur.v);
+                juur = paremPööre(juur);
+            }
+        } else if (tasakaal == -2){
+            if (juur.p.v == null || (juur.p.p != null && leiaKõrgus(juur.p.v) - leiaKõrgus(juur.p.p) <= 0)){
+                juur = vasakPööre(juur);
+            } else {
+                juur.p = paremPööre(juur.p);
+                juur = vasakPööre(juur);
+            }
+        }
+
         return juur;
     }
 
-    public static int leiaTasakaal(KOTipp juur){
+    /**
+     * Leiab puu kõrguse
+     * @param juur Puu
+     * @return Kõrgus
+     */
+    public static int leiaKõrgus(KOTipp juur){
         if (juur == null){
             return 0;
         }
 
+
         if (juur.p != null && juur.v != null){
-            return Math.max(leiaTasakaal(juur.p), leiaTasakaal(juur.v));
+            return Math.max(leiaKõrgus(juur.p), leiaKõrgus(juur.v)) + 1;
         } else if (juur.p != null){
-            return leiaTasakaal(juur.p) + 1;
+            return leiaKõrgus(juur.p) + 1;
         } else if (juur.v != null){
-            return leiaTasakaal(juur.v) + 1;
+            return leiaKõrgus(juur.v) + 1;
         } else {
             return 1;
         }
     }
 
-
+    /**
+     * Eemaldab kirje AVL-puust
+     * @param juur Puu
+     * @param väärtus Kirje
+     * @return Uus puu
+     */
     public static KOTipp eemaldaKirje(KOTipp juur, int väärtus) {
-        throw new UnsupportedOperationException();
-    }
-
-    public static KOTipp liidaAVLpuud(KOTipp avl1, KOTipp avl2) {
-        throw new UnsupportedOperationException();
-    }
-
-    public static String prindi(KOTipp juur){
-        String s = "";
-        s += juur.väärtus;
-
-        if (juur.v != null && juur.p != null){
-            s += "(" + prindi(juur.v) + "," + prindi(juur.p) + ")";
-        } else if (juur.v != null){
-            s += "(" + prindi(juur.v) + ")";
-        } else if (juur.p != null){
-            s += "(" + prindi(juur.p) + ")";
+        if (juur == null){
+            return juur;
         }
-        return s;
+
+        if (juur.p != null && juur.p.väärtus == väärtus){
+            juur.p = eemaldaKirje(juur.p, väärtus);
+        } else if (juur.v != null && juur.v.väärtus == väärtus){
+            juur.v = eemaldaKirje(juur.v, väärtus);
+        } else if (juur.väärtus == väärtus){
+            if (juur.p != null && juur.v != null){
+                KOTipp temp = otsiJärgnev(juur.p, juur.väärtus);
+                if (temp.väärtus != väärtus)eemaldaKirje(juur, temp.väärtus);
+                if (temp.väärtus == väärtus)eemaldaKirje(juur.p, temp.väärtus);
+                juur.väärtus = temp.väärtus;
+            } else if (juur.p != null){
+                return juur.p;
+            } else if (juur.v != null){
+                return juur.v;
+            } else {
+                return null;
+            }
+        }else {
+            if (väärtus >= juur.väärtus){
+                juur.p = eemaldaKirje(juur.p, väärtus);
+            } else {
+                juur.v = eemaldaKirje(juur.v, väärtus);
+            }
+        }
+
+        int tasakaal = 0;
+        if (juur.v != null && juur.p != null){
+            tasakaal = leiaKõrgus(juur.v) - leiaKõrgus(juur.p);
+        } else if (juur.v != null){
+            tasakaal = leiaKõrgus(juur.v);
+        } else if (juur.p != null){
+            tasakaal = -leiaKõrgus(juur.p);
+        }
+
+        if (tasakaal == 2){
+            if (juur.v.p == null || (juur.v.v != null && leiaKõrgus(juur.v.v) - leiaKõrgus(juur.v.p) >= 0)){
+                juur = paremPööre(juur);
+            } else {
+                juur.v = vasakPööre(juur.v);
+                juur = paremPööre(juur);
+            }
+        } else if (tasakaal == -2){
+            if (juur.p.v == null || (juur.p.p != null && leiaKõrgus(juur.p.v) - leiaKõrgus(juur.p.p) <= 0)){
+                juur = vasakPööre(juur);
+            } else {
+                juur.p = paremPööre(juur.p);
+                juur = vasakPööre(juur);
+            }
+        }
+
+
+        return juur;
     }
 
-    public static void main(String[] args) {
-        /*
-        KOTipp j1 = new KOTipp(1);
-        KOTipp j2 = new KOTipp(2);
-        KOTipp j3 = new KOTipp(3);
-        KOTipp j4 = new KOTipp(4);
-        KOTipp j5 = new KOTipp(5);
-        KOTipp j6 = new KOTipp(6);
-        KOTipp j7 = new KOTipp(7);
-        KOTipp j8 = new KOTipp(8);
-        KOTipp j9 = new KOTipp(9);
+    /**
+     * Otsib väärtusele järgneva väärtuse puust
+     * @param juur Puu
+     * @param väärtus Väärtus, millele järgnev otsida
+     * @return Juur, mis on väärtusele järgnev
+     */
+    public static KOTipp otsiJärgnev(KOTipp juur, int väärtus){
+        if (juur == null){
+            return juur;
+        }
 
-        j8.p = j9;
-        j8.v = j6;
-        j6.p = j7;
-        j6.v = j2;
-        j2.v = j1;
-        j2.p = j4;
-        j4.v = j3;
-        j4.p = j5;
-         */
+        if (juur.väärtus == väärtus+1){
+            return juur;
+        }
 
-        KOTipp juur = new KOTipp(40);
-        lisaKirje(juur, 35);
-        lisaKirje(juur, 36);
-        lisaKirje(juur, 60);
-        lisaKirje(juur, 44);
-        lisaKirje(juur, 41);
-        lisaKirje(juur, 55);
-        lisaKirje(juur, 44);
-        lisaKirje(juur, 43);
-        lisaKirje(juur, 45);
+        KOTipp r1 = null;
+        KOTipp r2 = null;
+        int min = juur.väärtus;
+        if (juur.v != null){
+            r1 = otsiJärgnev(juur.v, väärtus);
+            min = Math.min(min, r1.väärtus);
+        }
+        if (juur.p != null){
+            r2 = otsiJärgnev(juur.p, väärtus);
+            min = Math.min(min, r2.väärtus);
+        }
 
-        System.out.println(prindi(juur));
-        System.out.println("----------");
-        KOTipp juur2 = lisaKirje(juur, 10);
-        System.out.println(prindi(juur2));
-        System.out.println("a");
+        if (min < väärtus) return null;
+        if (juur.väärtus == min) return juur;
+        if (r1.väärtus == min) return r1;
+        if (r2.väärtus == min) return r2;
+        return null;
+    }
+
+    /**
+     * Liisab kaks AVL-puud kokku
+     * @param avl1 Esimene puu
+     * @param avl2 Teine puu
+     * @return Kombineeritud puu
+     */
+    public static KOTipp liidaAVLpuud(KOTipp avl1, KOTipp avl2) {
+        ArrayList<Integer> j1 = puuJärjendiks(avl1);
+        ArrayList<Integer> j2 = puuJärjendiks(avl2);
+
+        int i1 = 0;
+        int i2 = 0;
+        int[] kokku = new int[j1.size() + j2.size()];
+        for (int i = 0; i < kokku.length; i++) {
+            if (i1 < j1.size() && i2 < j2.size()){
+                if (j1.get(i1) <= j2.get(i2)){
+                    kokku[i] = j1.get(i1);
+                    i1++;
+                } else {
+                    kokku[i] = j2.get(i2);
+                    i2++;
+                }
+            } else if (i1 < j1.size()){
+                kokku[i] = j1.get(i1);
+                i1++;
+            } else if (i2 < j2.size()){
+                kokku[i] = j2.get(i2);
+                i2++;
+            }
+        }
+
+        return järjendPuuks(kokku, 0, kokku.length-1);
+    }
+
+    /**
+     * Muudab puu järjendiks
+     * @param juur Puu
+     * @return Järjend
+     */
+    public static ArrayList<Integer> puuJärjendiks(KOTipp juur){
+        if (juur == null){
+            return new ArrayList<>();
+        }
+
+        ArrayList<Integer> j = new ArrayList<>();
+        j.addAll(puuJärjendiks(juur.v));
+        j.add(juur.väärtus);
+        j.addAll(puuJärjendiks(juur.p));
+
+        return j;
+    }
+
+    /**
+     * Muudab järjendi AVL-puuks
+     * @param a Täisarvude järjend
+     * @param start Start-indeks
+     * @param lõpp Lõpp-indeks
+     * @return Puu
+     */
+    public static KOTipp järjendPuuks(int[] a, int start, int lõpp){
+        if (start > lõpp){
+            return null;
+        }
+
+        int keskmine = start+(lõpp - start)/2;
+        KOTipp juur = new KOTipp(a[keskmine]);
+        juur.v = järjendPuuks(a, start, keskmine-1);
+        juur.p = järjendPuuks(a, keskmine+1, lõpp);
+
+        return juur;
+    }
+
+    /**
+     * Teeb puul parempöörde
+     * @param juur Puu
+     * @return Pööratud puu
+     */
+    public static KOTipp paremPööre(KOTipp juur){
+        if (juur == null) return null;
+
+        KOTipp juur1 = juur;
+        juur = new KOTipp(juur1.v.väärtus);
+        juur.p = new KOTipp(juur1.väärtus);
+        juur.p.v = juur1.v.p;
+        juur.v = juur1.v.v;
+        juur.p.p = juur1.p;
+        juur.x = leiaKõrgus(juur);
+        return juur;
+    }
+
+    /**
+     * Teeb puul vasakpöörde
+     * @param juur Puu
+     * @return Pööratud puu
+     */
+    public static KOTipp vasakPööre(KOTipp juur){
+        if (juur == null) return null;
+
+        KOTipp juur1 = juur;
+        juur = new KOTipp(juur1.p.väärtus);
+        juur.v = new KOTipp(juur1.väärtus);
+        juur.v.v = juur1.v;
+        juur.v.p = juur1.p.v;
+        juur.p = juur1.p.p;
+        juur.x = leiaKõrgus(juur);
+        return juur;
     }
 }
---
+++ AA2024S/K6/Kodu6a.java
@@ -0,0 +1,139 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Kodu6a {
+
+    public static Tipp koostaKoodipuu(String sisu) {
+        // a) teksti põhjal luua sümbolite sagedustabel;
+        // b) sagedustabeli põhjal luua loetelu ühetipulistest puudest (igas tipus on siis info kujul sümbol + selle esinemiste arv)
+        // c) selliste puude metsast Huffmani koodipuu loomine (koodipuu lehttippudes peaks olema .info välja väärtuseks vastav sümbol)
+
+        List<Tipp> jarjekord = new ArrayList<>();
+        for (int i = 0; i < sisu.length(); i++) {
+            String s = Character.toString(sisu.charAt(i));
+            Boolean leitud = false;
+            System.out.println(jarjekord.size());
+            for (Tipp t : jarjekord){
+                if (Objects.equals(t.info, s)){
+                    t.x += 1;
+                    leitud = true;
+                    break;
+                }
+            }
+            if (!leitud) {
+                Tipp t1 = new Tipp(s);
+                t1.x = 1;
+                jarjekord.add(t1);
+            }
+        }
+
+        List<Tipp> minHeap = new ArrayList<>();
+        for (Tipp t : jarjekord){
+            lisaPuule(minHeap, t);
+        }
+
+        Tipp r1 = null;
+        for (int i = 0; i < jarjekord.size(); i++) {
+            Tipp r = new Tipp(null);
+            if ()
+        }
+
+        return null;
+    }
+
+    public static List<Tipp> lisaPuule(List<Tipp> puu, Tipp t){
+        puu.add(t);
+        int idx = puu.size()-1;
+        while (idx > 0 && puu.get((idx-1)/2).x > puu.get(idx).x){
+            Tipp temp = puu.get(idx);
+            puu.set(idx, puu.get((idx-1)/2));
+            puu.set((idx-1)/2, temp);
+            idx = (idx-1)/2;
+        }
+        return puu;
+    }
+
+    public static List<Tipp> emmaldaKirje(List<Tipp> puu, Tipp t){
+        int idx = -1;
+        for (int i = 0; i < puu.size(); i++) {
+            if (puu.get(i) == t){
+                puu.set(i, puu.getLast());
+                idx = i;
+                break;
+            }
+        }
+
+        puu.removeLast();
+
+        while (true){
+            int v = 2*idx+1;
+            int p = 2*idx+2;
+            int vaikseim = idx;
+            if (v < puu.size() && puu.get(v).x < puu.get(idx).x){
+                vaikseim = v;
+            }
+            if (p < puu.size() && puu.get(p).x < puu.get(idx).x){
+                vaikseim = p;
+            }
+            if (vaikseim != idx){
+                
+            }
+        }
+    }
+
+
+    public static boolean[] kodeeri(Tipp koodipuu, String sisu) {
+        // d) leida koodipuust igale sümbolile vastav uus kood -> prefikskood
+        // e) algtekst, prefikskoodid, koodipuu -> pakitud tekst (bittide massiiv, kus true on 1 ja false on 0)
+
+        throw new UnsupportedOperationException();
+    }
+
+    public static String dekodeeri(Tipp koodipuu, boolean[] bitid) {
+        // f) koodipuu, pakitud tekst -> lahtipakitud tekst
+
+        throw new UnsupportedOperationException();
+    }
+
+    public static Tipp lisaKirje(Tipp juur, String väärtus) {
+        if (juur == null){
+            Tipp temp = new Tipp(väärtus);
+            temp.x = 1;
+            return temp;
+        }
+
+
+
+        return juur;
+    }
+
+    public static void main(String[] args) throws IOException {
+        // Näide meetodite rakendamisest
+
+        // Tekstifailist algse teksti lugemine
+        String sisu = Files.readString(new File("Kõrboja_peremees.txt").toPath());
+
+        // Koodipuu koostamine, kodeerimine ja dekodeerimine
+        Tipp koodipuu = koostaKoodipuu(sisu);
+//        boolean[] bitid = kodeeri(koodipuu, sisu);
+//        String dekodeeritud = dekodeeri(koodipuu, bitid);
+//        System.out.println(dekodeeritud);
+
+        // Näide abiklassi FailiSisu kasutamisest
+
+        // Andmete faili kirjutamine
+        // Tipp koodipuu = koostaKoodipuu(sisu);
+        // boolean[] bitid = kodeeri(koodipuu, sisu);
+        // FailiSisu.kirjutaFaili(new File("kodeeritud.dat"), koodipuu, bitid);
+
+        // Andmete failist lugemine
+        // FailiSisu failiSisu = FailiSisu.loeFailist(new File("kodeeritud.dat"));
+        // String dekodeeritud = dekodeeri(failiSisu.koodipuu, failiSisu.bitid);
+        // System.out.println(dekodeeritud);
+    }
+
+}
