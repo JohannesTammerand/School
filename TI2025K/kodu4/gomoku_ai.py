a3daac5e9665dc259f71a1dacc2b6167958641ea
johan <johannestammerand@gmail.com>
2025-04-18 17:44:05 +0300

    04/18/2025 17:44:05

--- TI2025K/kodu4/gomoku_ai.py
+++ TI2025K/kodu4/gomoku_ai.py
@@ -10,28 +10,51 @@
 # millele antakse argumendina ette mängijat tähistav number (1 - must ehk X; 2 - valge ehk O) 
 # ning mis tagastab selle mängija järgmise käigu koordinaatide tuple'ina (rida, veerg)
 
+import random
 import gomoku_baas as gb
 import gomoku as gm
 from copy import deepcopy
 
 
-def getTurn(board, player):
-    moves = gm.getPossMoves(gm.board)
+player = 1
 
+def getTurn(board, move, depth=2):
+    print("getTurn")
+    global player
+    player = move
 
-    bestMove = None
+    moves = gm.getPossMoves(board)
+    bestTurns = random.choice(moves)
     bestRate = 0
-    for move in moves:
-        rating = rateMove(move, player)
-        if rating == 100:
-            return move
+    for turn in moves:
+        b = deepcopy(board)
+        b[turn[0]][turn[1]] = player
+        _turn, rate = minimax(b, depth, move, float('-inf'), float('inf'))
+        if rate > bestRate:
+            bestTurns = [turn]
+            bestRate = rate
+        elif rate == bestRate:
+            bestTurns.append(turn)
 
-        if (rating > bestRate):
-            bestMove = move
-            bestRate = rating
-            #print(bestMove, bestRate)
+    return random.choice(bestTurns)
 
-    return bestMove
+
+    # moves = gm.getPossMoves(gm.board)
+
+
+    # bestMove = None
+    # bestRate = 0
+    # for move in moves:
+    #     rating = rateMove(move, player)
+    #     if rating == 100:
+    #         return move
+
+    #     if (rating > bestRate):
+    #         bestMove = move
+    #         bestRate = rating
+    #         #print(bestMove, bestRate)
+
+    # return bestMove
 
 
     # # Üks võimalikest käikudest
@@ -41,7 +64,8 @@ def getTurn(board, player):
     # return move
 
 
-def rateMove(move, player):
+def rateMove(turn, move):
+    #print("rateMove")
     dirPairs = [((-1, 0), (1, 0)), ((0, -1), (0, 1)), ((-1, 1), (1, -1)), ((1, 1), (-1, -1))]
 
     score = 0
@@ -49,11 +73,11 @@ def rateMove(move, player):
         ownCount = 0
         enemyCount = 0
         for dir in pair:
-            pos = [move[0], move[1]]
+            pos = [turn[0], turn[1]]
             pos[0] += dir[0]
             pos[1] += dir[1]
             #print(pos[0], pos[1], move[0], move[1])
-            while (pos[0] > 0 and pos[1] > 0 and pos[0] < 14 and pos[1] < 14 and gm.board[pos[0]][pos[1]] == player):
+            while (pos[0] > 0 and pos[1] > 0 and pos[0] < 14 and pos[1] < 14 and gm.board[pos[0]][pos[1]] == move):
                 ownCount += 1
                 #print('leidis')
 
@@ -61,11 +85,11 @@ def rateMove(move, player):
                 pos[1] += dir[1]
 
 
-            pos = [move[0], move[1]]
+            pos = [turn[0], turn[1]]
             pos[0] += dir[0]
             pos[1] += dir[1]
             #print(pos[0], pos[1], move[0], move[1])
-            while (pos[0] > 0 and pos[1] > 0 and pos[0] < 14 and pos[1] < 14 and gm.board[pos[0]][pos[1]] == 3 - player):
+            while (pos[0] > 0 and pos[1] > 0 and pos[0] < 14 and pos[1] < 14 and gm.board[pos[0]][pos[1]] == 3 - move):
                 enemyCount += 1
                 #print('leidis')
 
@@ -82,27 +106,122 @@ def rateMove(move, player):
 
     return score
 
+def rateBoard(board, move):
+    total = 0
+    for i in range(15):
+        for j in range(15):
+            if board[i][j] == move:
+                total += rateStone((i, j), move, board)
+            elif board[i][j] == 3 - player:
+                total -= rateStone((i, j), 3 - move, board)
+    return total
 
-def minimax(board, depth, move, player):
-    bestTurn = (-1, -1)
-    bestRate = 0
+
+def rateStone(pos, move, board):
+    directions = [(-1, 0), (0, -1), (-1, 1), (1, 1)]
+    score = 0
+    for dx, dy in directions:
+        line = getLineFromBoard(pos, dx, dy, board, move)
+        score += scorePattern(line)
+    return score
+
+def getLineFromBoard(pos, dx, dy, board, move):
+    line = ''
+    x, y = pos
+
+    for offset in range(-4, 5):
+        i = x + offset * dx
+        j = y + offset * dy
+        if 0 <= i < 15 and 0 <= j < 15:
+            val = board[i][j]
+            if val == move:
+                line += 'X'
+            elif val == 0:
+                line += '_'
+            else:
+                line += 'O'
+        else:
+            line += 'O'
+    return line
+
+def scorePattern(line):
+    patterns = {
+        'XXXXX': 100000,
+        '_XXXX_': 10000,
+        'XXXX_': 5000, '_XXXX': 5000,
+        '_XXX_': 1000, 'XXX__': 500, '__XXX': 500,
+        '_XX_': 200, 'XX__': 100, '__XX': 100
+    }
+
+    total = 0
+    for pattern, score in patterns.items():
+        total += line.count(pattern) * score
+    return total
+
+
+
+def minimax(board, depth, move, alpha, beta):
+    print("minimax")
     moves = gm.getPossMoves(board)
+    bestTurn = moves[0]
+    bestRate = 0
+    
     depth -= 1
     if depth >= 0 and len(moves) > depth:
         if player == move:
-            pass
+            bestTurn, bestRate = maximizer(board, moves, depth, move, alpha, beta)
+        else:
+            bestTurn, bestRate = minimizer(board, moves, depth, move, alpha, beta)
+    return bestTurn, bestRate
+    
 
-def maximizer(board, moves, move):
-    bestTurn = (-1, -1)
-    bestRate = 0
-    bestTurns = []
-    for turn in moves:
+def maximizer(board, allturns, depth, move, alpha, beta):
+    print("maximizer")
+    bestturn = (-1, -1)
+    bestturns = allturns
+    bestrate = 100
+    for turn in allturns:
         b = deepcopy(board)
         b[turn[0]][turn[1]] = move
-        rate = rateMove(move, b)
-        if rate > bestRate:
-            bestTurns = [turn]
+        rate = rateBoard(b, move)
+        if rate==100 and bestrate<100:
+            bestturns = [turn]
+            bestrate = rate
+        if depth > 0 and bestrate<100:
+            _turn, rate = minimax(b, depth, 3 - move, alpha, beta)
+        if rate > bestrate:
+            bestrate = rate
+            bestturns = [turn]
+        elif rate == bestrate:
+            bestturns.append(turn)
+
+        alpha = max(alpha, rate)
+        if beta <= alpha:
+            break
+
+    bestturn = random.choice(bestturns)
+    return bestturn, bestrate
+
+def minimizer(board, allturns, depth, move, alpha, beta):
+    print("minimizer")
+    bestRate = 100
+    bestTurns = allturns
+
+    for turn in allturns:
+        b = deepcopy(board)
+        b[turn[0]][turn[1]] = move
+        if depth > 0:
+            _turn, rate = minimax(b, depth, 3 - move, alpha, beta)
+        else:
+            rate = 60 - rateBoard(b, move)
+        if rate < bestRate:
             bestRate = rate
+            bestTurns = [turn]
         elif rate == bestRate:
             bestTurns.append(turn)
-    return bestTurn, bestRate
+
+        beta = min(beta, rate)
+        if beta <= alpha:
+            break
+
+    return random.choice(bestTurns), bestRate
